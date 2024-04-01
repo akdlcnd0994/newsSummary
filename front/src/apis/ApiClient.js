d04d0d9e3a526bab3efa518798cb93bc18976ed2
양휘준 <akdlcnd0994@naver.com>
2024-04-01 13:11:17 +0900

    +

--- front/src/apis/ApiClient.js
+++ front/src/apis/ApiClient.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const apiClient = axios.create({ baseURL: "http://localhost:8080" });
+export const apiClient = axios.create({ baseURL: "http://ec2-13-125-106-127.ap-northeast-2.compute.amazonaws.com:8080/" });
